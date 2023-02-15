eb309ef77d0f98e94b26ce26a4fc94e3d6418ce4
Claire Poulsen <claire.e.poulsen@gmail.com>
2023-02-15 15:00:59 -0700

    did a thing

--- App.js
+++ App.js
@@ -1,13 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import React, { Component } from 'react';
+import FileList from './components/FileList';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lastFileAdded: null,
+    }
+  }
+
+  createNewFile = async () => {
+    // random-ish filename
+    let timestamp = new Date().toISOString().replace(/[-:.]/g, "");
+    let rando = ("" + Math.random()).substring(2, 8);
+    let rFileName = timestamp + rando + '.txt';
+
+    await FileSystem.writeAsStringAsync(
+      FileSystem.documentDirectory + rFileName,
+      "This is the file " + rFileName
+    );
+    this.setLastFileAdded(rFileName);
+  }
+
+  setLastFileAdded(filename) {
+    this.setState ({
+      lastFileAdded: filename,
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.container}>
+          <Text>These are my files.</Text>
+          <Button title="Generate"
+            onPress={this.createNewFile} />
+        </View>
+        <View style={styles.container}>
+          <FileList
+            directoryUri={FileSystem.documentDirectory}
+            forceUpdate={this.state.lastFileAdded}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +55,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    },
+})
---
+++ components/FileList.js
@@ -0,0 +1,94 @@
+import { Alert, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import React, { Component } from 'react';
+
+export default class FileList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            directoryListing: null,
+        }
+    }
+
+    async componentDidMount() {
+        this.grabListing(this.props.directoryUri);
+    }
+
+    grabListing = async (uri) => {
+        const listing = await FileSystem.readDirectoryAsync(uri);
+        this.setDirectoryListing(listing);
+    }
+
+    setDirectoryListing = (listing) => {
+        this.setState({
+            directoryListing: listing,
+        })
+    }
+
+    async componentDidUpdate() {
+        this.grabListing(this.props.directoryUri);
+    }
+
+    async deleteFile(name, key) {
+        Alert.alert(name + " - " + key);
+        const f2del = await
+        FileSystem.deleteAsync(FileSystem.documentDirectory + name);
+    }
+
+    async showFile(name, key) {
+        try {
+            const stringFromFile = await
+            FileSystem.readAsStringAsync(FileSystem.documentDirectory + name);
+            Alert.alert("Read: " + stringFromFile);
+        } catch (error) {
+            Alert.alert("Read error");
+        }
+    }
+
+    render() {
+        if (!this.state.directoryListing) {
+            return (
+                <Text>There are no files here!</Text>
+            );
+        } else {
+            return (
+                <ScrollView style={styles.fileList}>
+                    {
+                        this.state.directoryListing.map(
+                            (name, key) => (
+                                <Pressable
+                                    onPress={() => {this.showFile(name, key)}}
+                                    onLongPress={() => {this.deleteFile(name, key)}}
+                                >
+                                    <Text key={key} style={styles.textyText}>{ name }</Text>
+                                </Pressable>
+                            )
+                        )
+                    }
+                </ScrollView>
+            );
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      },
+      fileList: {
+        width: '90%',
+      },
+      textyText: {
+        fontSize: 15,
+        lineHeight: 40,
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 5,
+        margin: 5,
+        padding: 2,
+      },
+  })
+  
